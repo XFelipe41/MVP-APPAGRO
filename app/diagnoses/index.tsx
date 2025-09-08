83e46f08c7f2e30340e06dbca8a06a33db74e09a
XFelipe41 <jfcaicedot@ut.edu.co>
2025-09-08 11:13:44 -0500

    se mejoro la hora

--- app/diagnoses/index.tsx
+++ app/diagnoses/index.tsx
@@ -94,7 +94,7 @@ export default function DiagnosesListScreen() {
                     year: 'numeric', month: 'long', day: 'numeric'
                   })}{' - '}
                   {new Date(item.date).toLocaleTimeString("es-ES", {
-                    hour: '2-digit', minute: '2-digit'
+                    hour: '2-digit', minute: '2-digit', hour12: true
                   })}
                 </ThemedText>
               </TouchableOpacity>
